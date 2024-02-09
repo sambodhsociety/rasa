05ab0e49796c12135d05a8138ec64f396f591bad
Chityhi <chityhi@gmail.com>
2024-02-08 23:12:18 -0500

    Build back to top arrow

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,17 @@
 document.getElementById('year').innerHTML = new Date().getFullYear()
+
+const scrollHeight = 300
+
+const topArrow = document.querySelector('.top-arrow')
+
+const scrollContainer = () => {
+  return document.documentElement || document.body
+}
+
+document.addEventListener('scroll', () => {
+  if (scrollContainer().scrollTop > scrollHeight) {
+    topArrow.classList.remove('hidden')
+  } else {
+    topArrow.classList.add('hidden')
+  }
+})
